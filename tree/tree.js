8f1c899a6e9e2b80fdd6986b7fe9ecbc2c7e7124
이진웅 <jinung91@gmail.com>
2025-01-07 19:16:11 +0900

    feat: 250107 트리 자료구조, 이진 탐색 트리 공부 중

---
+++ tree/tree.js
@@ -0,0 +1,124 @@
+function preorder(nodes, idx) {
+  if (idx < nodes.length) {
+    let ret = `${nodes[idx]}`;
+    ret += preorder(nodes, idx * 2 + 1);
+    ret += preorder(nodes, idx * 2 + 2);
+    return ret;
+  }
+
+  return '';
+}
+
+function inorder(nodes, idx) {
+  if (idx < nodes.length) {
+    let ret = inorder(nodes, idx * 2 + 1);
+    ret += `${nodes[idx]}`;
+    ret += inorder(nodes, idx * 2 + 2);
+    return ret;
+  }
+
+  return '';
+}
+
+function postorder(nodes, idx) {
+  if (idx < nodes.length) {
+    let ret = postorder(nodes, idx * 2 + 1);
+    ret += postorder(nodes, idx * 2 + 2);
+    ret += `${nodes[idx]}`;
+    return ret;
+  }
+
+  return '';
+}
+
+// console.log(inorder([1, 2, 3, 4, 5, 6, 7], 0));
+
+// 이진탐색트리 노드
+class Node {
+  constructor(key) {
+    this.left = null;
+    this.right = null;
+    this.val = key;
+  }
+}
+
+// 이진 탐색 트리 클래스
+class BST {
+  constructor() {
+    // 루트 노드를 생성자에서 만들어 줌
+    this.root = null;
+  }
+
+  // 탐색 트리 생성 메서드
+  insert(key) {
+    // 루트 노드가 없으면 새로운 노드 생성
+    if (!this.root) {
+      this.root = new Node(key);
+    } else {
+      // 루트 노드가 있다면, 루트 노드를 현재 루트 노드 기준으로 설정
+      let curr = this.root;
+
+      while (true) {
+        // 들어온 값이 현재 루트 노드의 밸류보다 작으면 왼쪽 노드로 배치
+        if (key < curr.val) {
+          // 현재 노드에 왼쪽 노드에 값이 있으면, 루트 노드의 기준을 왼쪽으로 옮겨줌
+          if (curr.left) {
+            curr = curr.left;
+          } else {
+            // 값이 없으면 새로운 노드를 생성하고 반복문 탈출
+            curr.left = new Node(key);
+            break;
+          }
+        } else {
+          // 오른쪽 노드 생성, 방식은 위와 동일
+          if (curr.right) {
+            curr = curr.right;
+          } else {
+            curr.right = new Node(key);
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  // 탐색 메서드
+  search(key) {
+    let curr = this.root;
+
+    // 노드가 존재하고, 찾으려는 값이 현재 노드의 밸류랑 같지 않으면 반복함
+    while (curr && curr.val !== key) {
+      // 만약 현재 루트 노의 밸류보다 찾는 값이 작다면, 왼쪽 노드로 현재 루트 노드를 이동
+      if (key < curr.val) {
+        curr = curr.left;
+      } else {
+        // 반대의 경우 오른쪽 노드로 이동
+        curr = curr.right;
+      }
+    }
+
+    return curr;
+  }
+}
+
+function bstTest(list, searchList) {
+  const bst = new BST();
+  const result = [];
+
+  for (const key of list) {
+    bst.insert(key);
+  }
+
+  for (const key of searchList) {
+    if (bst.search(key)) {
+      result.push(true);
+    } else {
+      result.push(false);
+    }
+  }
+
+  return result;
+}
+
+bstTest([5, 3, 8, 4, 2, 1, 7, 10], [1, 2, 5, 6]);
+// bstTest([1, 3, 5, 7, 9], [2, 4, 6, 8, 1]);
