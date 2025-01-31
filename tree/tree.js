475f10271ad6993b56bb49e143dce9d54cefe3b4
이진웅 <jinung91@gmail.com>
2025-01-31 18:39:47 +0900

    feat: 250131 트리, bfs

--- tree/tree.js
+++ tree/tree.js
@@ -177,9 +177,69 @@ class Queue {
 }
 
 function isValidMove(ny, nx, n, m, maps) {
-  return ny >= 0 && ny > n && nx >= 0 && nx > m && maps[ny][nx] !== 'X';
+  return ny >= 0 && ny < n && nx >= 0 && nx < m && maps[ny][nx] !== 'X';
 }
 
-function escapeFromMaze(maps) {}
+function appendToQueue(ny, nx, k, time, visited, q) {
+  if (!visited[ny][nx][k]) {
+    visited[ny][nx][k] = true;
+    q.push([ny, nx, k, time + 1]);
+  }
+}
 
-escapeFromMaze(['SOOOL', 'XXXXO', 'OOOOO', 'OXXXX', 'OOOOE']);
+function escapeFromMaze(maps) {
+  const n = maps.length;
+  const m = maps[0].length;
+  const visited = Array.from(Array(n), () =>
+    Array(m)
+      .fill(false)
+      .map(() => Array(2).fill(false))
+  );
+
+  const dy = [-1, 1, 0, 0];
+  const dx = [0, 0, -1, 1];
+  const q = new Queue();
+  let endY = -1;
+  let endX = -1;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (maps[i][j] === 'S') {
+        q.push([i, j, 0, 0]);
+        visited[i][j][0] = true;
+      }
+
+      if (maps[i][j] === 'E') {
+        endY = i;
+        endX = j;
+      }
+    }
+  }
+
+  while (!q.isEmpty()) {
+    const [y, x, k, time] = q.pop();
+    console.log(y, x, k, time);
+
+    if (y === endY && x === endX && k === 1) {
+      return time;
+    }
+
+    for (let i = 0; i < 4; i++) {
+      const ny = y + dy[i];
+      const nx = x + dx[i];
+
+      if (!isValidMove(ny, nx, n, m, maps)) {
+        continue;
+      }
+      if (maps[ny][nx] === 'L') {
+        appendToQueue(ny, nx, 1, time, visited, q);
+      } else {
+        appendToQueue(ny, nx, k, time, visited, q);
+      }
+    }
+  }
+
+  return -1;
+}
+
+console.log(escapeFromMaze(['SOOOL', 'XXXXO', 'OOOOO', 'OXXXX', 'OOOOE']));
